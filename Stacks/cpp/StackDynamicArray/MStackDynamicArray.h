745eebb03ccd88bb760d69b60e2c946ddaa85a2b
Matt <mlh@hougiepi.(none)>
2015-01-22 14:44:11 -0800

    Factored out ResizeArray code, MStackDynamicArray

--- Stacks/cpp/StackDynamicArray/MStackDynamicArray.h
+++ Stacks/cpp/StackDynamicArray/MStackDynamicArray.h
@@ -7,105 +7,106 @@
 
 template <class SomeType>
 class MStackDynamicArray {
-  private:
-    int stackSize;
-    int maxStackSize;
-    SomeType *start;
-
   public:
+    // Constructors/Destructor
     MStackDynamicArray();
-    MStackDynamicArray(int);    // Initialize a maximum size for the stack.
+    MStackDynamicArray(int);    // Set an initial maximum size for the stack.
     ~MStackDynamicArray();
-    bool isEmpty();
-    SomeType& pop();
-    void push(const SomeType &A);
+    // Methods
+    bool IsEmpty();
+    SomeType& Pop();
+    void Push(const SomeType &A);
     int size();
-    SomeType top();
+    SomeType Top();
+
+  private:
+    // Methods
+    void ResizeArray(int);
+    // Data members
+    int max_size_;  // Max capacity of the backing array.
+    int size_;  // Number of items currently in the backing array.
+    SomeType *start_;  // The backing array.
 };
 
 template <class SomeType>
-MStackDynamicArray<SomeType>::MStackDynamicArray() {
-  stackSize = 0;
-  maxStackSize = DEFAULT_MSTACK_SIZE;
-  start = new SomeType[DEFAULT_MSTACK_SIZE];
+MStackDynamicArray<SomeType>::MStackDynamicArray()
+    : max_size_(DEFAULT_MSTACK_SIZE),
+      size_(0) {
+  start_ = new SomeType[DEFAULT_MSTACK_SIZE];
 }
 
 // Allow initialization with a specified max stack size.
 template <class SomeType>
-MStackDynamicArray<SomeType>::MStackDynamicArray(int maxStackSize) {
-  stackSize = 0;
-  this->maxStackSize = maxStackSize;
-  start = new SomeType[maxStackSize];
+MStackDynamicArray<SomeType>::MStackDynamicArray(int max_size)
+    : max_size_(max_size),
+      size_(0) {
+  start_ = new SomeType[max_size_];
 }
 
 template <class SomeType>
 MStackDynamicArray<SomeType>::~MStackDynamicArray() {
-  // Avoid memory leaks! Delete the array pointed to by start.
-  delete[] start;
+  // Avoid memory leaks! Delete the array pointed to by start_.
+  delete[] start_;
 }
 
 template <class SomeType>
-bool MStackDynamicArray<SomeType>::isEmpty() {
-  return (stackSize == 0 ? true : false);
+bool MStackDynamicArray<SomeType>::IsEmpty() {
+  return (size_ == 0 ? true : false);
 }
 
 template <class SomeType>
-SomeType& MStackDynamicArray<SomeType>::pop() {
-  if (stackSize > 0) {
-    stackSize--;
-    // Shrink the array by 1/2 if a pop() causes it to be <= 1/4 full.
-    if (stackSize <= maxStackSize / 4) {
-      // Create a new (smaller) array, copy the items over, then delete the
-      // larger old one.
-      SomeType *newStart = new SomeType[maxStackSize / 2];
-      // Loop until stackSize + 1, as we already decremented stackSize.
-      for (int i = 0; i < stackSize + 1; i++) {
-        newStart[i] = start[i];
-      }
-      delete[] start;  // Delete the old array.
-      start = newStart;  // Make start point to the new array.
+SomeType& MStackDynamicArray<SomeType>::Pop() {
+  if (size_ > 0) {
+    // Shrink the array by 1/2 if a Pop() causes it to be < 1/4 full.
+    if (size_ - 1 < max_size_ / 4) {
+      MStackDynamicArray<SomeType>::ResizeArray(max_size_ / 2);
     }
-    return start[stackSize];
+    return start_[--size_];
   }
   else {
     throw "Stack is empty!";
   }
 }
 
+// Create a new array of size new_size, copy all the items from the start_
+// array into it, set max_size_ = new_size, and make start_ point to the
+// new array.
 template <class SomeType>
-SomeType MStackDynamicArray<SomeType>::top() {
-  return (stackSize > 0 ? start[(int) stackSize - 1] : NULL);
+void MStackDynamicArray<SomeType>::ResizeArray(int new_size) {
+  SomeType *new_start = new SomeType[new_size];
+  // Copy existing stack items to the new array.
+  for (int i = 0; i < size_; i++) {
+    new_start[i] = start_[i];
+  }
+  delete[] start_;  // Delete the old array.
+  start_ = new_start;  // Make start_ point to the new array.
+  max_size_ = new_size;
 }
 
 template <class SomeType>
-void MStackDynamicArray<SomeType>::push(const SomeType &A) {
-  if (stackSize == maxStackSize) {
-    // We'll set the limit at (2^31)-1 items.
-    if ( (maxStackSize*2) > LONG_MAX ) {
-      printf("Stack size limit exceeded\n");
+SomeType MStackDynamicArray<SomeType>::Top() {
+  return (size_ > 0 ? start_[size_ - 1] : NULL);
+}
+
+template <class SomeType>
+void MStackDynamicArray<SomeType>::Push(const SomeType &A) {
+  if (size_ == max_size_) {
+    if ( (max_size_*2) > LONG_MAX ) {
       throw "Stack size limit exceeded";
     }
     else {
       // Double the size of the array.
-      maxStackSize *= 2;
-      // If not enough memory, "new" will throw an exception.
-      SomeType *newStart = new SomeType[maxStackSize];
-      // Copy old array contents to new array.
-      for (int i = 0; i < stackSize; i++) {
-        newStart[i] = start[i];
-      }
-      delete[] start;  // Delete the old array.
-      start = newStart;  // Make start point to the new array.
+      MStackDynamicArray<SomeType>::ResizeArray(max_size_ * 2);
     }
   }
-
-  start[stackSize] = A;
-  stackSize++;
+  // Insert the item into the array.
+  start_[size_] = A;
+  size_++;
 }
 
 template <class SomeType>
 int MStackDynamicArray<SomeType>::size() {
-  return stackSize;
+  return size_;
 }
 
 #endif
--- Stacks/cpp/StackDynamicArray/main.cc
+++ Stacks/cpp/StackDynamicArray/main.cc
@@ -8,32 +8,31 @@
 #include "MStackDynamicArray.h"
 
 int main(int argc, char** argv) {
-  MStackDynamicArray<std::string> myStack(4);
+  MStackDynamicArray<std::string> myStack(2);
   printf("Pushing these values to the stack:");
   std::vector<std::string> v {"I", "am", "on", "the", "stack"};
   for (std::vector<std::string>::iterator it = v.begin();
        it != v.end(); it++) {
     printf(" \"%s\"", it->c_str());
-    myStack.push(*it);  // 5th push should double underlying array size to 8.
+    myStack.Push(*it); 
   }
   printf("\nAFTER PUSHES: Stack size: %d, with top value: \"%s\"\n",
-         myStack.size(), myStack.top().c_str());
+         myStack.size(), myStack.Top().c_str());
 
   printf("\nPopping stack until empty.\n");
-  while (!myStack.isEmpty()) {
+  while (!myStack.IsEmpty()) {
     // DON'T DO THIS - order of argument evaluation is not defined by the
-    // standard, thus pop() could be called before top(), resulting in
+    // standard, thus Pop() could be called before Top(), resulting in
     // different entries being printed.
     // printf("Popped entry: \"%s\" \"%s\"",
-    //        myStack.top().c_str(), myStack.pop().c_str());
+    //        myStack.Top().c_str(), myStack.Pop().c_str());
 
-    // Correct way to print val from top() first, then pop()... two statements.
-    // printf("Top: \"%s\", ", myStack.top().c_str());
-    printf("Popped entry: \"%s\", ", myStack.pop().c_str());
+    // Correct way to print val from Top() first, then Pop()... two statements.
+    // printf("Top: \"%s\", ", myStack.Top().c_str());
+    printf("Popped entry: \"%s\", ", myStack.Pop().c_str());
 
     // Print stack size too, just to for extra debug info.
     printf("stack size: %d\n", myStack.size());
   }
-
   return 0;
 }
